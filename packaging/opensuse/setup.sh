93a9c75d98d81a416a76c191ecb5c3a7bdd5bff0
Timothy Vaccarelli <tmvfroid@gmail.com>
2018-09-12 17:07:28 -0400

    Update OpenSUSE packaging scripts for 69.0.3497.81

--- packaging/opensuse/setup.sh
+++ packaging/opensuse/setup.sh
@@ -6,8 +6,13 @@ PACKAGING_DIR=$(dirname $(readlink -f $0))
 
 DOWNLOAD_CACHE=$(dirname $(readlink -f $0))/../../download_cache
 
+UNGOOGLED_DIR=$(dirname $(readlink -f $0))/../../../
+
 pushd $PACKAGING_DIR
 mkdir $DOWNLOAD_CACHE
+cp -R $UNGOOGLED_DIR/buildkit ./buildkit
+cp -R $UNGOOGLED_DIR/config_bundles .
+cp $UNGOOGLED_DIR/version.ini .
 python3 -m buildkit downloads retrieve -b config_bundles/$BUNDLE -c $DOWNLOAD_CACHE
 python3 -m buildkit downloads unpack -b config_bundles/$BUNDLE -c $DOWNLOAD_CACHE ../
 python3 -m buildkit prune -b config_bundles/$BUNDLE ../
@@ -16,15 +21,15 @@ python3 -m buildkit prune -b config_bundles/$BUNDLE ../
 #python3 -m buildkit domains apply -b config_bundles/$BUNDLE -c domainsubcache.tar.gz ../
 popd
 
-cp -r $PACKAGING_DIR/SOURCES/* ~/rpm/SOURCES/
+cp -r $PACKAGING_DIR/patches/* ~/rpm/SOURCES/
 
 pushd $PACKAGING_DIR/chromium-icons_contents
 tar cjf ~/rpm/SOURCES/chromium-icons.tar.bz2 *
 popd
 
-cp $PACKAGING_DIR/patches/*.patch ~/rpm/SOURCES
-cp $PACKAGING_DIR/patches/*/*.patch ~/rpm/SOURCES
-cp $PACKAGING_DIR/patches/*/*/*.patch ~/rpm/SOURCES
 cp $PACKAGING_DIR/ungoogled-chromium.spec ~/rpm/SPECS/
 
-mv $PACKAGING_DIR/../../tree ~/rpm/BUILD/tree
+mv $PACKAGING_DIR/buildkit ..
+mv $PACKING_DIR/config_bundles ..
+mv $PACKING_DIR/version.ini ..
+mv $PACKAGING_DIR/.. ~/rpm/BUILD/tree
